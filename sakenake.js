ce98684731420ad655ca6ed6ea41a7c1ddd5c1d4
e42240950-RafifDwiPradipta <e42240950@student.polije.ac.id>
2025-09-10 15:00:36 +0700

    Latihan Java script

---
+++ sakenake.js
@@ -0,0 +1,11 @@
+alert("Hallo Anak Haram");
+ confirm("hiban gamtenk");
+ confirm("apakah sudah capek?") 
+ prompt("Pemuda Nyeni");
+Date("01-29-2006");
+parseInt("123");
+setTimeout("functionRafif,5000")
+function foo() {
+alert("mbuh cok mbulet");
+} foo(); a
+
